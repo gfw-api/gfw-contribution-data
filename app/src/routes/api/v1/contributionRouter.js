fa3fe9e504fa207212d219753383b868db9c638f
rrequero <rareq1987@gmail.com>
2016-09-05 12:28:53 +0200

    add language to send confirmation mail

--- app/src/routes/api/v1/contributionRouter.js
+++ app/src/routes/api/v1/contributionRouter.js
@@ -21,6 +21,18 @@ class ContributionRouter {
         });
         logger.debug('Sending mail...');
         mailService.sendMail(config.get('wriMail.template'), this.request.body, wriRecipients);
+
+        // send mail to USER
+        let language = 'en';
+        if (this.request.body.loggedUser && this.request.body.loggedUser.language) {
+            language = this.request.body.loggedUser.language.toLowerCase().replace(/_/g, '-');
+        }
+        let template = `${config.get('userMail.template')}-${language}`;
+        mailService.sendMail(template, this.request.body, [{
+            address: this.request.body.mail
+        }]);
+
+
         this.body = '';
     }
 
